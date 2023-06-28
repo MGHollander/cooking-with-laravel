8c9f95b0190696f1245eb9f98ff4b5da43eee2e3
Marc Hollander <marchollander@gmail.com>
2023-06-28 21:45:35 +0200

    Enable Laravel strict mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Enable Laravel's strict mode for development
+        // @see https://coderflex.com/blog/laravel-strict-mode-all-what-you-need-to-know#all-in-one
+        Model::shouldBeStrict(!$this->app->isProduction());
     }
 }
