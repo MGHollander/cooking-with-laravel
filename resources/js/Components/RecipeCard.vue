70e06f07fbf374dddeb4a2bdc07282e8fade5fdf
Marc Hollander <marchollander@gmail.com>
2023-09-16 00:30:00 +0200

    Add a custom recipe 404 page with suggestions

---
+++ resources/js/Components/RecipeCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+defineProps({
+  recipe: Object,
+});
+</script>
+<template>
+  <Link :key="recipe.id" :href="route('recipes.show', recipe.slug)" class="recipe-card md:col-span-4">
+    <div v-if="recipe.image" class="recipe-card-image">
+      <img :src="recipe.image" alt="A picture of '{{ recipe.title }}'" />
+    </div>
+
+    <h2 class="recipe-card-title">{{ recipe.title }}</h2>
+  </Link>
+</template>
---
+++ resources/js/Components/SearchBlock.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+
+let props = defineProps({
+  recipes: Object,
+  q: String,
+  size: String,
+});
+
+const form = useForm({
+  q: props.q ?? "",
+});
+</script>
+
+<template>
+  <div class="relative flex w-full items-center rounded-lg shadow-lg">
+    <img
+      class="absolute inset-0 h-full w-full rounded-lg object-cover shadow-lg"
+      src="https://picsum.photos/id/999/1000"
+    />
+
+    <form
+      class="relative flex w-full"
+      :class="{ 'p-4 sm:px-16 sm:py-8 md:px-32': size === 'small', 'px-4 py-8 sm:p-16 md:p-32': !size }"
+      @submit.prevent="form.get(route('search'))"
+    >
+      <input
+        v-model="form.q"
+        class="w-full rounded-l-md border-2 border-gray-300 bg-white p-2 text-sm sm:px-6 sm:py-4 md:text-base lg:text-lg"
+        placeholder="Waar heb je zin in?"
+        type="search"
+      />
+      <button
+        type="submit"
+        :disabled="form.processing"
+        class="rounded-r-md border-2 border-l-0 border-emerald-700 bg-emerald-700 p-2 text-sm text-white sm:px-6 sm:py-4 md:text-base lg:text-lg"
+      >
+        Zoeken
+      </button>
+    </form>
+  </div>
+</template>
