03c1f74138a38b4516448b5b4c00afd88ae5698e
Marc Hollander <marchollander@gmail.com>
2023-11-07 22:11:21 +0100

    Add extra settings to media disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,9 +45,11 @@ return [
         ],
 
         'media' => [
-            'driver' => 'local',
-            'root'   => storage_path('app/public/media'),
-            'url'    => env('APP_URL') . '/storage/media',
+            'driver'     => 'local',
+            'root'       => storage_path('app/public/media'),
+            'url'        => env('APP_URL') . '/storage/media',
+            'visibility' => 'public',
+            'throw'      => false,
         ],
 
         's3' => [
