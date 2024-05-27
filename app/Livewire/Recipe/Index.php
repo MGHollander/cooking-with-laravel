c04b15c6daa463eabd95860ce9b1608b188daba5
Marc Hollander <marchollander@gmail.com>
2024-05-27 08:40:52 +0200

    #80 Add search to Livewire version

--- app/Livewire/Recipe/Index.php
+++ app/Livewire/Recipe/Index.php
@@ -5,23 +5,30 @@ namespace App\Livewire\Recipe;
 use App\Models\Recipe;
 use Livewire\Component;
 use Livewire\WithPagination;
+use ProtoneMedia\LaravelCrossEloquentSearch\Search;
 
 class Index extends Component
 {
   use WithPagination;
 
+  public ?string $search = null;
+
   public function render()
   {
     // TODO use a resource instead of a query
     return view('livewire.recipe.index', [
-      'recipes' => Recipe::query()
+      'recipes' => Search::add(Recipe::class, ['title', 'ingredients', 'instructions', 'tags.name'])
         ->paginate(12)
+        ->beginWithWildcard()
+        ->search($this->search)
+        ->withQueryString()
         ->through(fn($recipe) => [
           'id'    => $recipe->id,
           'title' => $recipe->title,
           'slug'  => $recipe->slug,
           'image' => $recipe->getFirstMediaUrl('recipe_image', 'card'),
         ]),
+      // TODO Add open graph tags.
     ]);
   }
 }
--- resources/views/livewire/recipe/index.blade.php
+++ resources/views/livewire/recipe/index.blade.php
@@ -1,5 +1,19 @@
 <div>
-  <h1 class="mb-4 text-2xl font-bold md:text-3xl">Recipes</h1>
+  <div class="mb-4 flex items-center justify-between">
+    <h1 class="text-2xl font-bold md:text-3xl">
+      @if ($search && $recipes->total() > 0)
+        Found {{ $recipes->count() }} recipes for "{{ $search }}"
+      @elseif ($search && $recipes->total() === 0)
+        No recipes found
+      @else
+        Recipes
+      @endif
+    </h1>
+    <form class="flex items-center space-x-2">
+      <label>Search:</label>
+      <input type="search" wire:model.live.debounce="search" class="rounded border border-slate-300 px-3 py-2" />
+    </form>
+  </div>
 
   @if ($recipes->total() > 0)
     <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
@@ -13,7 +27,9 @@
         {{ $recipes->links() }}
       </div>
     @endif
+  @elseif ($search && $recipes->total() === 0)
+    <p>No recipes found for "{{ $search }}".</p>
   @else
-    <p>Je hebt nog geen recepten opgeslagen.</p>
+    <p>No recipes found.</p>
   @endif
 </div>
