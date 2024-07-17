6e60c87f075197d1859118c50dc7d05cb41da45f
Marc Hollander <marchollander@gmail.com>
2024-07-17 21:10:12 +0200

    #41 Make a component for the recipe image placeholder

---
+++ resources/views/components/icon/meal.blade.php
@@ -0,0 +1,30 @@
+<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+     viewBox="0 0 144 144" width="300" height="160" {{ $attributes  }}>
+    >
+    <path opacity="0.1"
+          d="M18.667,98C17.75,98,17,98,17,98c0,4,6.25,8,13,8h84c6.75,0,13-4,13-8c0,0-0.75,0-1.667,0"/>
+    <path opacity="0.1" d="M121,82c-11.719,7-12.969-5.155-22-0.874l-0.306-0.044L100,98h20l1.231-16H121z"/>
+    <line fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"
+          stroke-miterlimit="10" x1="10" y1="98" x2="134" y2="98"/>
+    <circle fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-miterlimit="10" cx="56" cy="64" r="4"/>
+    <path fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"
+          stroke-miterlimit="10"
+          d="M18.667,98C17.75,98,17,98,17,98c0,4,4.25,8,11,8h87c6.75,0,12-4,12-8c0,0-0.75,0-1.667,0H72H18.667z"/>
+    <polygon fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round"
+             stroke-linejoin="round"
+             stroke-miterlimit="10" points="122,72 98,72 100,98 120,98"/>
+    <path fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"
+          stroke-miterlimit="10" d="M99,80.995c9.031-4.281,10.281,7.438,22,1"/>
+    <path fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"
+          stroke-miterlimit="10" d="M33.878,63c-6.679-6.756,7.169-8.844,0-16"/>
+    <path fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"
+          stroke-miterlimit="10" d="M48.939,50c-3.339-4.222,3.584-5.528,0-10"/>
+    <path fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"
+          stroke-miterlimit="10" d="M65.869,54c-7.124-6.756,7.647-8.844,0-16"/>
+    <path fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"
+          stroke-miterlimit="10" d="M78.939,64c-3.339-4.222,3.584-5.528,0-10"/>
+    <path fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"
+          stroke-miterlimit="10" d="M89.763,98C87.801,81,73.438,68,56,68S24.199,81,22.237,98H89.763z"/>
+</svg>
