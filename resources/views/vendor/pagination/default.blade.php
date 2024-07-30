e5795992593132ed277ac145f9b78fff570b152a
Marc Hollander <marchollander@gmail.com>
2024-07-30 14:08:44 +0200

    #41 Update pagination icon color

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -7,7 +7,7 @@
                     <span aria-hidden="true">
                         <svg width="7" height="12" viewBox="0 0 7 12"
                              xmlns="http://www.w3.org/2000/svg" data-svg="pagination-previous">
-                            <polyline fill="none" stroke="#000" stroke-width="1.2" points="6 1 1 6 6 11"/>
+                            <polyline fill="none" stroke="currentColor" stroke-width="1.2" points="6 1 1 6 6 11"/>
                         </svg>
                     </span>
                 </li>
@@ -15,7 +15,7 @@
                 <li>
                     <a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">
                         <svg width="7" height="12" viewBox="0 0 7 12" xmlns="http://www.w3.org/2000/svg">
-                            <polyline fill="none" stroke="#000" stroke-width="1.2" points="6 1 1 6 6 11"/>
+                            <polyline fill="none" stroke="currentColor" stroke-width="1.2" points="6 1 1 6 6 11"/>
                         </svg>
                     </a>
                 </li>
@@ -45,7 +45,7 @@
                 <li>
                     <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">
                         <svg width="7" height="12" viewBox="0 0 7 12" xmlns="http://www.w3.org/2000/svg">
-                            <polyline fill="none" stroke="#000" stroke-width="1.2" points="1 1 6 6 1 11"/>
+                            <polyline fill="none" stroke="currentColor" stroke-width="1.2" points="1 1 6 6 1 11"/>
                         </svg>
                     </a>
                 </li>
@@ -53,7 +53,7 @@
                 <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
                     <span aria-hidden="true">
                         <svg width="7" height="12" viewBox="0 0 7 12" xmlns="http://www.w3.org/2000/svg">
-                            <polyline fill="none" stroke="#000" stroke-width="1.2" points="1 1 6 6 1 11"/>
+                            <polyline fill="none" stroke="currentColor" stroke-width="1.2" points="1 1 6 6 1 11"/>
                         </svg>
                     </span>
                 </li>
