dfb257136d157f7986c5c0fc4d4d707976a00cca
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2025-01-14 20:19:05 +0100

    #41 Remove another todo

--- resources/views/components/kocina/search-bar.blade.php
+++ resources/views/components/kocina/search-bar.blade.php
@@ -34,7 +34,6 @@
         </button>
 
         {{--
-        TODO Uncomment when filters are added.
         <button type="button" class="search-bar-filter-button">
             <span class="sr-only">Open filters</span>
             <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
