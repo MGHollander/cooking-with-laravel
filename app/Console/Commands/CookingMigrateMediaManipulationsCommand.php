2f87d894d028b9dc929ce287f64c6abfa7b50e7a
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2025-01-27 21:04:42 +0100

    #41 Added cooking:migrate-media-manipulations command

---
+++ app/Console/Commands/CookingMigrateMediaManipulationsCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class CookingMigrateMediaManipulationsCommand extends Command
+{
+    protected $signature = 'cooking:migrate-media-manipulations';
+
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+        $media = Media::where('collection_name', '=', 'recipe_image');
+        $bar   = $this->output->createProgressBar($media->count());
+
+        $bar->start();
+
+        $media->chunk(100, function ($mediaItems) use ($bar) {
+            $mediaItems->each(function (Media $media) use ($bar) {
+                $updated       = false;
+                $manipulations = $media->manipulations;
+                if (isset($manipulations['card']['manualCrop']) && is_string($manipulations['card']['manualCrop'])) {
+                    $manipulations['card']['manualCrop'] = array_map('intval', explode(',', $manipulations['card']['manualCrop']));
+                    $manipulations['card']['width']      = [300];
+
+                    $updated = true;
+                }
+
+                if (isset($manipulations['show']['manualCrop']) && is_string($manipulations['show']['manualCrop'])) {
+                    $manipulations['show']['manualCrop'] = array_map('intval', explode(',', $manipulations['show']['manualCrop']));
+                    $manipulations['show']['width']      = [600];
+
+                    $updated = true;
+                }
+
+                if ($updated) {
+                    $media->manipulations = $manipulations;
+                    $media->save();
+                }
+
+                $bar->advance();
+            });
+        });
+
+        $bar->finish();
+    }
+}
