0bf583aaa78c8228a60e8a7449296c87c2a826c5
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2025-02-03 20:56:37 +0100

    #41 Revert "#41 Disable laravel-vite-plugin"

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import {createRequire} from "node:module";
 import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
 import vue from "@vitejs/plugin-vue";
-// import laravel from "laravel-vite-plugin";
+import laravel from "laravel-vite-plugin";
 import {defineConfig} from "vite";
 
 const require = createRequire(import.meta.url);
@@ -9,10 +9,10 @@ const require = createRequire(import.meta.url);
 export default defineConfig({
   plugins: [
     ckeditor5({theme: require.resolve("@ckeditor/ckeditor5-theme-lark")}),
-    // laravel({
-    //   input: "resources/js/app.js",
-    //   refresh: true,
-    // }),
+    laravel({
+      input: "resources/js/app.js",
+      refresh: true,
+    }),
     vue({
       template: {
         transformAssetUrls: {
