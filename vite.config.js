e066860baaee0c2625138ccc48bb0e56b92996bd
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2025-02-03 20:52:50 +0100

    #41 Disable laravel-vite-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
 import vue from "@vitejs/plugin-vue";
-import laravel from "laravel-vite-plugin";
+// import laravel from "laravel-vite-plugin";
 import {defineConfig} from "vite";
 
 export default defineConfig({
   plugins: [
     ckeditor5(),
-    laravel({
-      input: "resources/js/app.js",
-      refresh: true,
-    }),
+    // laravel({
+    //   input: "resources/js/app.js",
+    //   refresh: true,
+    // }),
     vue({
       template: {
         transformAssetUrls: {
