6514e4799c89142f0c8c3dd57c60a885e04f1ace
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2025-02-03 20:55:40 +0100

    #41 Revert "#41 Remove ckeditor theme from vite config"

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
+import {createRequire} from "node:module";
 import ckeditor5 from "@ckeditor/vite-plugin-ckeditor5";
 import vue from "@vitejs/plugin-vue";
 // import laravel from "laravel-vite-plugin";
 import {defineConfig} from "vite";
 
+const require = createRequire(import.meta.url);
+
 export default defineConfig({
   plugins: [
-    ckeditor5(),
+    ckeditor5({theme: require.resolve("@ckeditor/ckeditor5-theme-lark")}),
     // laravel({
     //   input: "resources/js/app.js",
     //   refresh: true,
