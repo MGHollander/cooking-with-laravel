f5de89c499964ae528a86d1257414885fcf4b930
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2025-06-22 19:41:55 +0200

    Minor updates to CookingMigrateMediaManipulationsCommand

--- app/Console/Commands/CookingMigrateMediaManipulationsCommand.php
+++ app/Console/Commands/CookingMigrateMediaManipulationsCommand.php
@@ -9,7 +9,7 @@ class CookingMigrateMediaManipulationsCommand extends Command
 {
     protected $signature = 'cooking:migrate-media-manipulations';
 
-    protected $description = 'Command description';
+    protected $description = 'Migrate media manipulations to the new format';
 
     public function handle(): void
     {
@@ -24,14 +24,14 @@ class CookingMigrateMediaManipulationsCommand extends Command
                 $manipulations = $media->manipulations;
                 if (isset($manipulations['card']['manualCrop']) && is_string($manipulations['card']['manualCrop'])) {
                     $manipulations['card']['manualCrop'] = array_map('intval', explode(',', $manipulations['card']['manualCrop']));
-                    $manipulations['card']['width']      = [300];
+                    $manipulations['card']['width'] = [600];
 
                     $updated = true;
                 }
 
                 if (isset($manipulations['show']['manualCrop']) && is_string($manipulations['show']['manualCrop'])) {
                     $manipulations['show']['manualCrop'] = array_map('intval', explode(',', $manipulations['show']['manualCrop']));
-                    $manipulations['show']['width']      = [600];
+                    $manipulations['show']['width'] = [1200];
 
                     $updated = true;
                 }
