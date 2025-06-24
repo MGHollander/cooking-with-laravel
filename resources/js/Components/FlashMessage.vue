37396e7d81900481bc8290503dc49ddaaa66b550
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2025-06-24 10:10:53 +0200

    #140 Add FlashMessage component

---
+++ resources/js/Components/FlashMessage.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    required: true,
+    validator: (value) => ['error', 'success', 'warning'].includes(value)
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  fullWidth: {
+    type: Boolean,
+    default: false
+  }
+});
+</script>
+
+<template>
+  <div
+    class="flex justify-between rounded-lg py-4"
+    :class="{
+      'bg-red-100 text-red-700': type === 'error',
+      'bg-emerald-100 text-emerald-800': type === 'success',
+      'bg-yellow-100 text-yellow-700': type === 'warning',
+    }"
+  >
+    <div class="px-4" :class="{ 'mx-auto w-full max-w-7xl sm:px-6 lg:px-8': fullWidth }">
+      <p v-html="message" />
+    </div>
+  </div>
+</template>
