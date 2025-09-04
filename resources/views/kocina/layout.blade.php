ed834a7a75f0a22db9a22a5f6dd662f33381473d
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2025-09-04 20:22:27 +0200

    Remove more Matomo (piwik)

--- resources/views/kocina/layout.blade.php
+++ resources/views/kocina/layout.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
-<head>
+  <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
@@ -9,42 +9,50 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin />
     <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600&display=swap" />
     <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Leckerli+One&display=swap" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600&display=swap"
-          media="print" onload="this.media='all'" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Leckerli+One&display=swap" media="print"
-          onload="this.media='all'" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600&display=swap"
+      media="print"
+      onload="this.media='all'"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Leckerli+One&display=swap"
+      media="print"
+      onload="this.media='all'"
+    />
     <noscript>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Leckerli+One&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Leckerli+One&display=swap" />
     </noscript>
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2.0">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2.0">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2.0">
-    <link rel="manifest" href="/site.webmanifest?v=2.0">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2.0" color="#eb4a36">
-    <link rel="shortcut icon" href="/favicon.ico?v=2.0">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2.0" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2.0" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2.0" />
+    <link rel="manifest" href="/site.webmanifest?v=2.0" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2.0" color="#eb4a36" />
+    <link rel="shortcut icon" href="/favicon.ico?v=2.0" />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff" />
 
     @vite(["resources/kocina/scss/app.scss", "resources/kocina/js/bootstrap.js"])
 
     {!! JsonLd::generate() !!}
-</head>
-<body>
-@include('piwik')
+  </head>
+  <body>
+    <x-kocina.navbar />
 
-<x-kocina.navbar />
+    {{ $slot }}
 
-{{ $slot }}
-
-<div class="container footer">
-    <div class="footer-text">
-        Ontstaan uit de passie voor programmeren van <a href="https://mghollander.nl" target="_blank">Marc</a>.
+    <div class="container footer">
+      <div class="footer-text">
+        Ontstaan uit de passie voor programmeren van
+        <a href="https://mghollander.nl" target="_blank">Marc</a>
+        .
+      </div>
+      <div class="footer-links"><a href="{{ route("privacy") }}">Privacy</a></div>
     </div>
-    <div class="footer-links"><a href="{{ route('privacy') }}">Privacy</a></div>
-</div>
 
-@stack("scripts")
-</body>
+    @stack("scripts")
+  </body>
 </html>
