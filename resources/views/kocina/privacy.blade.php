951d9d751319d9414b54e330fef4868bacd803f1
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2025-09-05 06:41:48 +0200

    Typo

--- resources/views/kocina/privacy.blade.php
+++ resources/views/kocina/privacy.blade.php
@@ -3,6 +3,6 @@
 
   <div class="container page">
     <h1 class="mb-4 text-2xl font-bold md:text-3xl">Privacy</h1>
-    <p class="mb-6">Ik verzamel geen persoonlijke gegevens en je privacy is gewaardborgd op deze website.</p>
+    <p class="mb-6">Ik verzamel geen persoonlijke gegevens en je privacy is gewaarborgd op deze website.</p>
   </div>
 </x-kocina.layout>
