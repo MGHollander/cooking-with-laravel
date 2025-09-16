31cf8ec15dc0cac91e20948890c5e0bbbd6d5461
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2025-09-16 20:18:15 +0200

    #24 Remove leftover sentry code

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,6 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Sentry\Laravel\Integration;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -45,7 +44,7 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            Integration::captureUnhandledException($e);
+            //
         });
     }
 }
