08e852471c84586b9a3eb8525547c6b8ce6af232
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2025-09-25 10:58:03 +0200

    #181 Add recipe edit button to overview

--- resources/views/components/kocina/recipe-card.blade.php
+++ resources/views/components/kocina/recipe-card.blade.php
@@ -3,22 +3,30 @@
   "title_tag" => "h3",
 ])
 
-<a href="{{ route("recipes.show", ["slug" => $recipe["slug"]]) }}" class="recipe-card recipe-card-link">
+<div class="recipe-card">
+  <a href="{{ route("recipes.show", ["slug" => $recipe["slug"]]) }}" class="recipe-card-link">
     <div class="recipe-card-media">
-        @if ($recipe["image"])
-            <img
-                src="{{ $recipe["image"] }}"
-                alt="{{ $recipe["title"] }}"
-                class="recipe-card-image"
-                width="300"
-                height="160"
-            />
-        @else
-            <x-icon.meal class="recipe-card-image-placeholder" />
-        @endif
+      @if ($recipe["image"])
+        <img
+          src="{{ $recipe["image"] }}"
+          alt="{{ $recipe["title"] }}"
+          class="recipe-card-image"
+          width="300"
+          height="160"
+        />
+      @else
+        <x-icon.meal class="recipe-card-image-placeholder" />
+      @endif
     </div>
 
     {{-- @formatter:off --}}
     <{{ $title_tag }} class="recipe-card-title">{{ $recipe["title"] }}</{{ $title_tag }}>
-</a>
-{{-- @formatter:on --}}
+    {{-- @formatter:on --}}
+  </a>
+
+  @auth
+    <a href="{{ route("recipes.edit", $recipe["id"]) }}" class="recipe-card-edit">
+      <x-icon.pen />
+    </a>
+  @endauth
+</div>
