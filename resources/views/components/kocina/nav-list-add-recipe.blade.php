9ae956802be124bc770cbcd3a956c132ccd866ce
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2026-02-01 11:34:46 +0100

    #266 fix: remove unnecessary user authentication parameter from recipe creation route

--- resources/views/components/kocina/nav-list-add-recipe.blade.php
+++ resources/views/components/kocina/nav-list-add-recipe.blade.php
@@ -1,4 +1,4 @@
 <ul {{ $attributes }}>
-    <x-kocina.nav-list-item label="{{ __('nav.add_recipe') }}" :route="route('recipes.create', auth()->user())" />
+    <x-kocina.nav-list-item label="{{ __('nav.add_recipe') }}" :route="route('recipes.create')" />
     <x-kocina.nav-list-item label="{{ __('nav.import_recipe') }}" :route="route('import.index')" />
 </ul>
